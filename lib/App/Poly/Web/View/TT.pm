7ce13682aecd08e3047de00d82702cc504d0b66a
John SJ Anderson <genehack@genehack.org>
2010-12-26 19:21:38 -0500

    use helper to generate TT-based View class

---
+++ lib/App/Poly/Web/View/TT.pm
@@ -0,0 +1,13 @@
+package App::Poly::Web::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+1;
